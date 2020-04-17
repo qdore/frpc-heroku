0e2bbf6a848ba6fa222d4b409da38500229109d5
KeithSu <KeithSuLove@gmail.com>
2020-04-17 11:48:45 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd /frpc
+
+cat <<-EOF > /frpc/frpc.ini
+[common]
+token=12345
+server_addr = www.keithsu.ga
+server_port = 7000
+
+[http_hero]
+type = tcp
+remote_port = 6060
+plugin = http_proxy
+
+EOF
+
+/frpc/frpc -c /frpc/frpc.ini
