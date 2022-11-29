2dc8be473ac3db6a05800891556e1b374873f716
Wei Qiang <13wei.qiang@gmail.com>
2022-11-29 12:52:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,11 @@ server_port = ${port}
 [ssh]
 type = tcp
 local_ip = 127.0.0.1
-local_port = 6000
+local_port = $PORT
 remote_port = 16000
 
 EOF
 
 /frpc/frpc -c /frpc/frpc.ini &
 
-/ss/shadowsocks2-linux -s 'ss://chacha20-ietf-poly1305:adore@:6000'
+/ss/shadowsocks2-linux -s "ss://chacha20-ietf-poly1305:adore@:$PORT"
